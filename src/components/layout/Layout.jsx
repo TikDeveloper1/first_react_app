7cd39500506b5df14add7c727a7cae17fc9b2e4a
TikDeveloper1 <tikdevjs@gmail.com>
2022-07-18 21:37:10 +0400

    Lesson 3 State and Lifecicles

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Counter } from '../counter/Counter'
 import Footer from '../footer/Footer'
 import Header from '../header/Header'
 
-const Layout = ({x,y,z,children}) => {
+const Layout = ({children}) => {
+  const [isShowCounter,setIsShowCounter] = useState(false);
+
+  const toggleCounter = () => setIsShowCounter(prev => !prev);
 
   return (
    <>
     <Header />
+    <div style={{margin: "25px 0"}}>
+      {isShowCounter && <Counter/>}
+      <button onClick={toggleCounter}> {isShowCounter? "Hide Counter" : "Show Counter"} </button>
+    </div>
+
     <main>
-        <h1>{x}</h1>
-        <h1>{y}</h1>
-        <h1>{z}</h1>
         {children}
     </main>
     <Footer/>
